f2e88a765c571661989e8badcd32784130db6e2a
kjaehnig <jaehnig.k@gmail.com>
2024-04-20 01:15:44 -0500

    bkend test

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,8 +17,7 @@ def process_image(image_data):
         image_data = image_data[:, :, :3]
     gryimg = cv2.cvtColor(image_data, cv2.COLOR_BGR2GRAY)
     invimg = gryimg
-    img = cv2.resize(invimg, (28, 28),
-               interpolation=cv2.INTER_AREA)
+    img = cv2.resize(invimg, (28, 28))
     # img = cv2.cvtColor(img, cv2.COLOR_RGBA2GRAY)
     # img = np.expand_dims(img, axis=-1) / 255.0
     img = img[np.newaxis, :]
