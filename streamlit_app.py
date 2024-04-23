5621370e2331c49b05af8bf0b041535b57ea4d2c
kjaehnig <jaehnig.k@gmail.com>
2024-04-23 02:17:32 -0500

    new site layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -86,7 +86,8 @@ def predict_digit_from_canvas(canvas_data, num_samples):
         # Prediction
         # pred = model.predict(img, batch_size=num_samples)  # Assume model.predict handles BNN sampling
         pred = np.array([model(img).numpy() for ii in range(num_samples)])
-        pred = np.percentile(pred, 50, axis=1)  # Median over samples
+        st.write(pred.shape)
+        pred = np.percentile(pred, 50, axis=0)  # Median over samples
         pred_digit = np.argmax(pred)
 
         return img, pred, pred_digit
