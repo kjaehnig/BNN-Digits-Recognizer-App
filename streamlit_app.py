dcfb05068c133428664195990b1152f262c3fabd
Karl Jaehnig <jaehnig.k@gmail.com>
2024-04-25 19:03:43 -0400

    integraded contour digit code

--- streamlit_app.py
+++ streamlit_app.py
@@ -183,7 +183,7 @@ def predict_digit_from_canvas(canvas_data, num_samples):
             st.image(img[0].reshape(28,28,1), 
                 clamp=True,
                 use_column_width='always')
-            pred = np.array([model(img[0]).numpy().squeeze() for ii in range(num_samples)])
+            pred = np.array([model(img[0].reshape(28,28,1)).numpy().squeeze() for ii in range(num_samples)])
             pred = np.sum(pred, axis=0) / num_samples
             # pred = np.exp(pred) / np.sum(np.exp(pred))
             pred_digit = np.argmax(pred)
