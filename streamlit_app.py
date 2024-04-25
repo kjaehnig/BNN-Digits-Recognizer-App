e1b5052859fe983d3eccb83e3da1e94fe97d1abd
Karl Jaehnig <jaehnig.k@gmail.com>
2024-04-24 21:23:13 -0400

    new layout

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,7 +121,7 @@ def clear_selection():
 col1, col2 = st.columns(2)
 with col1:
     with st.container():
-        st.write("Draw a single digit b/w 0-9")
+        st.write("Draw a single digit between 0-9")
         # Streamlit canvas for drawing digits
         canvas_result = st_canvas(
             stroke_width=10, 
@@ -137,7 +137,7 @@ with col1:
 with st.sidebar:
     st.header("Control Panel")
     # Sampling number input
-    N = st.slider('N (Number of Samplings)', min_value=0, max_value=50, value=1)
+    N = st.slider('N (Number of samples)', min_value=0, max_value=50, value=1)
     if N > 10:
         st.warning("Setting N above 10 may slow down the predictions.")
 
@@ -148,8 +148,9 @@ if pred_digit is None:
 img = None
     # Button to submit the drawing for prediction
 if st.button('Submit'):
-    img, pred, pred_digit = predict_digit_from_canvas(canvas_result.image_data, N)
-    st.write(f"Predicted digit: {pred_digit}")
+    with st.spinner("Processing and predicting digit from image...")
+        img, pred, pred_digit = predict_digit_from_canvas(canvas_result.image_data, N)
+        st.write(f"Predicted digit: {pred_digit}")
 
 with col2:
     if img is not None:
@@ -159,8 +160,10 @@ with col2:
                 clamp=True,
                 use_column_width='always')
 
-            st.write("Probabilities")
-            st.bar_chart(data=pred.squeeze())
+if img is not None:
+    with st.container():
+        st.write("Probabilities across possible digits")
+        st.bar_chart(data=pred.squeeze())
 
 
 def register_prediction_checkbox():
