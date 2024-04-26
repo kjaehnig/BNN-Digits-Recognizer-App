2e2526f09ff68fb5009b4ae9fee1313f5a75313e
Karl Jaehnig <jaehnig.k@gmail.com>
2024-04-25 20:12:57 -0400

    layout fixes

--- streamlit_app.py
+++ streamlit_app.py
@@ -238,7 +238,7 @@ if img is not None and plot_all_preds:
         else:
             st.write("**Probabilities across possible digits**")
         for ii in range(pred.shape[0]):
-            st.bar_chart(data=pred.squeeze().T[ii])
+            st.bar_chart(data=pred.squeeze()[ii].T)
 
 
 def register_prediction_checkbox():
