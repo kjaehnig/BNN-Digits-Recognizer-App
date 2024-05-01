af5f570645e3770e5d4836d816c509a08d515cb4
kjaehnig <jaehnig.k@gmail.com>
2024-04-30 20:33:51 -0500

    new cv specs

--- cv_contour_to_mnist.py
+++ cv_contour_to_mnist.py
@@ -43,10 +43,10 @@ for contour in contours:
 
     if h > w:  # More height than width, pad width
         pad_size = abs((h - w)) // 2
-        digit = cv2.copyMakeBorder(digit, pad_size//2, pad_size//2, pad_size, pad_size, cv2.BORDER_CONSTANT, value=[255,255,255])
+        digit = cv2.copyMakeBorder(digit, pad_size//2, pad_size//2, pad_size, pad_size, cv2.BORDER_CONSTANT, value=[0,0,0])
     else:  # More width than height, pad height
         pad_size = abs(w - h) // 2
-        digit = cv2.copyMakeBorder(digit, pad_size, pad_size, pad_size//2, pad_size//2, cv2.BORDER_CONSTANT, value=[255,255,255])
+        digit = cv2.copyMakeBorder(digit, pad_size, pad_size, pad_size//2, pad_size//2, cv2.BORDER_CONSTANT, value=[0,0,0])
     # Resize to 28x28
     resized = cv2.resize(digit, (28, 28), interpolation=cv2.INTER_AREA)
     digit_images.append(resized)
